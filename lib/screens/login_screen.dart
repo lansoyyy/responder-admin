fe45a5b64be62e8473b8e1c94968238ef79ff919
Lans12345 <olanalans12345@gmail.com>
2023-09-11 18:56:18 +0800

    wip login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/image 1.png',
+              height: 250,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,3 +1,3 @@
 import 'package:flutter/material.dart';
 
-var primary = Colors.amber[800];
+var primary = const Color(0Xff18DF98);
