5e96cec9795113cf227a79cda29e1caab6e07e8f
Lans12345 <olanalans12345@gmail.com>
2023-09-11 19:18:52 +0800

    added initial analytics tab

--- lib/screens/tabs/analytics_tab.dart
+++ lib/screens/tabs/analytics_tab.dart
@@ -1,10 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:responder_admin/widgets/text_widget.dart';
 
 class AnalyticsTab extends StatelessWidget {
   const AnalyticsTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TextWidget(
+                text: 'Analytics',
+                fontSize: 18,
+                fontFamily: 'Bold',
+                color: Colors.black,
+              ),
+              const SizedBox(
+                width: 800,
+              ),
+              Align(
+                alignment: Alignment.topRight,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.notifications,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.account_circle,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const Divider(
+            color: Colors.black,
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Container(
+            width: 600,
+            height: 200,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          TextWidget(
+            text: 'Reports',
+            fontSize: 18,
+            fontFamily: 'Bold',
+            color: Colors.black,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            width: 600,
+            height: 200,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
