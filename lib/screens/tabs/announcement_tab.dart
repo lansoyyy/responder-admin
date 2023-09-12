f698affca29f959f7a558f7ddae064ac33626f9b
Lans12345 <olanalans12345@gmail.com>
2023-09-12 18:38:26 +0800

    wip anouncement tab

--- lib/screens/tabs/announcement_tab.dart
+++ lib/screens/tabs/announcement_tab.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:responder_admin/utils/colors.dart';
 
 class AnnouncementTab extends StatelessWidget {
   const AnnouncementTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: primary,
+          onPressed: () {},
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          )),
+    );
   }
 }
