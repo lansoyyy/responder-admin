d6bc4a572a8e1a69022db15e1289c3cc5586f6f8
Lans12345 <olanalans12345@gmail.com>
2023-09-21 19:37:55 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:responder_admin/screens/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          authDomain: 'responder-67a90.firebaseapp.com',
+          apiKey: "AIzaSyA12fFunT5kbj2TmPZIBiSjImXsoPM3pi",
+          appId: "1:697586667913:web:f4701a7d869d013082d158",
+          messagingSenderId: "697586667913",
+          projectId: "responder-67a90",
+          storageBucket: "responder-67a90.appspot.com"));
   runApp(const MyApp());
 }
 
