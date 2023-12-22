303a7e0d5bde7f28aee4e0c29a71961265299328
Lans12345 <olanalans12345@gmail.com>
2023-12-22 11:32:59 +0800

    fixed message

--- lib/screens/tabs/messages_tab.dart
+++ lib/screens/tabs/messages_tab.dart
@@ -17,82 +17,90 @@ class _MessagesTabState extends State<MessagesTab> {
   String msg = '';
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: SizedBox(
-        width: 500,
-        height: 500,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextWidget(
-              text: 'Chat',
-              fontSize: 19,
-              fontFamily: 'Bold',
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            StreamBuilder<QuerySnapshot>(
-                stream: FirebaseFirestore.instance
-                    .collection('Message')
-                    .snapshots(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.hasError) {
-                    print(snapshot.error);
-                    return const Center(child: Text('Error'));
-                  }
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    print('waiting');
-                    return const Padding(
-                      padding: EdgeInsets.only(top: 50),
-                      child: Center(
-                          child: CircularProgressIndicator(
-                        color: Colors.black,
-                      )),
-                    );
-                  }
+    return Center(
+      child: SingleChildScrollView(
+        child: SizedBox(
+          width: 750,
+          height: 700,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextWidget(
+                text: 'Chat',
+                fontSize: 19,
+                fontFamily: 'Bold',
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('Message')
+                      .orderBy('dateTime')
+                      .snapshots(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.hasError) {
+                      print(snapshot.error);
+                      return const Center(child: Text('Error'));
+                    }
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      print('waiting');
+                      return const Padding(
+                        padding: EdgeInsets.only(top: 50),
+                        child: Center(
+                            child: CircularProgressIndicator(
+                          color: Colors.black,
+                        )),
+                      );
+                    }
 
-                  final data1 = snapshot.requireData;
-                  return SizedBox(
-                    height: 350,
-                    width: 500,
-                    child: ListView.builder(
-                      itemCount: data1.docs.length,
-                      itemBuilder: (context, index1) {
-                        return ListTile(
-                          leading: const Icon(
-                            Icons.account_circle_outlined,
-                          ),
-                          title: TextWidget(
-                            text: data1.docs[index1]['msg'],
-                            fontSize: 18,
-                          ),
-                          subtitle: TextWidget(
-                            text: data1.docs[index1]['name'],
-                            fontSize: 14,
-                          ),
-                        );
+                    final data1 = snapshot.requireData;
+                    return SizedBox(
+                      height: 350,
+                      width: 500,
+                      child: ListView.builder(
+                        itemCount: data1.docs.length,
+                        itemBuilder: (context, index1) {
+                          return ListTile(
+                            leading: const Icon(
+                              Icons.account_circle_outlined,
+                            ),
+                            title: TextWidget(
+                              text: data1.docs[index1]['msg'],
+                              fontSize: 18,
+                            ),
+                            subtitle: TextWidget(
+                              text: data1.docs[index1]['name'],
+                              fontSize: 14,
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  }),
+              TextFormField(
+                controller: msgController,
+                decoration: InputDecoration(
+                    hintText: 'Type your message here...',
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        if (msgController.text != '') {
+                          addMessage('Admin', msgController.text);
+                          showToast('Message sent!');
+                          msgController.clear();
+                        } else {
+                          showToast('Please input a message');
+                        }
                       },
-                    ),
-                  );
-                }),
-            TextFormField(
-              controller: msgController,
-              decoration: InputDecoration(
-                  hintText: 'Type your message here...',
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      addMessage('Admin', msgController.text);
-                      showToast('Message sent!');
-                      msgController.clear();
-                    },
-                    icon: const Icon(
-                      Icons.send,
-                    ),
-                  )),
-            ),
-          ],
+                      icon: const Icon(
+                        Icons.send,
+                      ),
+                    )),
+              ),
+            ],
+          ),
         ),
       ),
     );
